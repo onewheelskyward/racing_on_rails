1f3f4d0be38dcf4835a2a248ff88576debd563b6
Scott Willson <scott.willson@gmail.com>
2011-07-26 19:18:53 -0700

    Time travel to different homepages in dev

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,6 +7,8 @@ class HomeController < ApplicationController
   # * upcoming_events: instance of UpcomingEvents with default parameters
   # * recent_results: Events with Results within last two weeks
   def index
+    time_travel if Rails.env.development?
+
     @upcoming_events = UpcomingEvents.find_all(:weeks => RacingAssociation.current.weeks_of_upcoming_events)
 
     cutoff = Date.today - RacingAssociation.current.weeks_of_recent_results * 7
@@ -15,8 +17,8 @@ class HomeController < ApplicationController
       :select => "DISTINCT(events.id), events.name, events.parent_id, events.date, events.sanctioned_by",
       :joins => [:races => :results],
       :conditions => [
-        'events.date > ? and events.sanctioned_by = ?', 
-        cutoff, RacingAssociation.current.default_sanctioned_by
+        'events.date > ? and events.date < ? and events.sanctioned_by = ?', 
+        cutoff, Time.zone.now.tomorrow, RacingAssociation.current.default_sanctioned_by
       ],
       :order => 'events.date desc'
     )
@@ -30,5 +32,25 @@ class HomeController < ApplicationController
     expires_in 1.hour, :public => true
 
     render_page
+  end
+  
+  
+  private
+  
+  def time_travel
+    if params[:date].present?
+      t = Date.parse(params[:date])
+      Timecop.travel(t)
+    else 
+      Timecop.return
+    end
+
+    if params[:weeks_of_recent_results].present?
+      RacingAssociation.current.weeks_of_recent_results = params[:weeks_of_recent_results].to_i
+    end
+
+    if params[:weeks_of_upcoming_events].present?
+      RacingAssociation.current.weeks_of_upcoming_events = params[:weeks_of_upcoming_events].to_i
+    end
   end
 end
