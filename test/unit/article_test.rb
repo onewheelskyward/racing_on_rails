ef8704b91a8bdfa2dc2d0553ae7dd082ce8d032d
Scott Willson <scott.willson@gmail.com>
2011-11-05 21:14:21 -0700

    Fix article test

--- test/unit/article_test.rb
+++ test/unit/article_test.rb
@@ -3,13 +3,14 @@ require File.expand_path("../../test_helper", __FILE__)
 # :stopdoc:
 class ArticleTest < ActiveSupport::TestCase
   test "move in list" do
-    article = FactoryGirl.create(:article)
-    article_2 = FactoryGirl.create(:article)
+    article_category = FactoryGirl.create(:article_category)
+    article = FactoryGirl.create(:article, :article_category => article_category)
+    article_2 = FactoryGirl.create(:article, :article_category => article_category)
     
     article.move_to_bottom
-    assert_equal [ article_2, article ], Article.all, "Should be sorted"
+    assert_equal [ article_2, article ], Article.all.sort, "Should be sorted"
     
     article_2.move_to_bottom
-    assert_equal [ article, article_2 ], Article.all, "Should be sorted"
+    assert_equal [ article, article_2 ], Article.all.sort, "Should be sorted"
   end
 end
