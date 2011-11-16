99d38c3501040c50559184d81bead0ea961eb2ce
Scott Willson <scott.willson@gmail.com>
2011-11-16 08:49:33 -0800

    Fix officials test

--- test/acceptance/acceptance_test.rb
+++ test/acceptance/acceptance_test.rb
@@ -5,8 +5,14 @@ require "capybara/rails"
 require "minitest/autorun"
 
 class AcceptanceTest < ActiveSupport::TestCase
+  DOWNLOAD_DIRECTORY = "/tmp/webdriver-downloads"
+
   include Capybara::DSL
   
+  Capybara.register_driver :selenium do |app|
+    Capybara::Selenium::Driver.new(app, :browser => :chrome, :switches => ["--user-data-dir=#{Rails.root}/tmp/chrome-profile"])
+  end
+
   # Selenium tests start the Rails server in a separate process. If test data is wrapped in a
   # transaction, the server won't see it.
   DatabaseCleaner.strategy = :truncation
@@ -15,7 +21,7 @@ class AcceptanceTest < ActiveSupport::TestCase
   
   def report_error_count
     unless @passed
-      puts page.source
+      save_and_open_page
     end
   end
   
@@ -41,6 +47,19 @@ class AcceptanceTest < ActiveSupport::TestCase
     end
   end
 
+  def wait_for_download(glob_pattern)
+    raise ArgumentError if glob_pattern.blank? || (glob_pattern.respond_to?(:empty?) && glob_pattern.empty?)
+    begin
+      Timeout::timeout(10) do
+        while Dir.glob("#{DOWNLOAD_DIRECTORY}/#{glob_pattern}").empty?
+          sleep 0.25
+        end
+      end
+    rescue Timeout::Error => e
+      raise Timeout::Error, "Did not find '#{glob_pattern}' in #{DOWNLOAD_DIRECTORY} within seconds 10 seconds. Found: #{Dir.entries(DOWNLOAD_DIRECTORY).join(", ")}"
+    end
+  end
+
   # Go to login page and login
   def login_as(person)
     visit "/person_session/new"
@@ -58,10 +77,20 @@ class AcceptanceTest < ActiveSupport::TestCase
     Discipline.reset
   end
   
+  def remove_download(filename)
+    FileUtils.rm_f "#{DOWNLOAD_DIRECTORY}/#{filename}"
+  end
+
   def set_driver
     # if ENV["CAPYBARA_DRIVER"] == "selenium"
       Capybara.current_driver = :selenium
     # end
+    FileUtils.rm_rf "#{Rails.root}/tmp/chrome-profile"
+    FileUtils.mkdir_p "#{Rails.root}/tmp/chrome-profile"
+    FileUtils.cp_r "#{Rails.root}/test/chrome-profile", "#{Rails.root}/tmp"
+
+    FileUtils.rm_rf DOWNLOAD_DIRECTORY
+    FileUtils.mkdir_p DOWNLOAD_DIRECTORY
   end
   
   def reset_capybara_driver
