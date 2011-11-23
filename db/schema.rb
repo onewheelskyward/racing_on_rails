28e4b24bcd5999f0f7de8693b8a35fd94dd5f673
Scott Willson <scott.willson@gmail.com>
2011-11-23 10:11:07 -0800

    Dump current MySQL schema to schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110806162623) do
+ActiveRecord::Schema.define(:version => 20111121165105) do
 
   create_table "adjustments", :force => true do |t|
     t.integer  "order_id"
@@ -88,7 +89,7 @@ ActiveRecord::Schema.define(:version => 20110806162623) do
 
   create_table "categories", :force => true do |t|
     t.integer  "position",                     :default => 0,   :null => false
-    t.string   "name",           :limit => 64,                  :null => false
+    t.string   "name",           :limit => 64, :default => "",  :null => false
     t.integer  "lock_version",                 :default => 0,   :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -128,6 +129,8 @@ ActiveRecord::Schema.define(:version => 20110806162623) do
   end
 
   add_index "discipline_bar_categories", ["category_id", "discipline_id"], :name => "discipline_bar_categories_category_id_index", :unique => true
+  add_index "discipline_bar_categories", ["category_id"], :name => "idx_category_id"
+  add_index "discipline_bar_categories", ["discipline_id"], :name => "idx_discipline_id"
 
   create_table "disciplines", :force => true do |t|
     t.string   "name",         :limit => 64, :default => "",    :null => false
@@ -165,6 +168,15 @@ ActiveRecord::Schema.define(:version => 20110806162623) do
   add_index "duplicates_people", ["duplicate_id"], :name => "index_duplicates_racers_on_duplicate_id"
   add_index "duplicates_people", ["person_id", "duplicate_id"], :name => "index_duplicates_racers_on_racer_id_and_duplicate_id", :unique => true
   add_index "duplicates_people", ["person_id"], :name => "index_duplicates_racers_on_racer_id"
+
+  create_table "duplicates_racers", :id => false, :force => true do |t|
+    t.integer "racer_id"
+    t.integer "duplicate_id"
+  end
+
+  add_index "duplicates_racers", ["duplicate_id"], :name => "index_duplicates_racers_on_duplicate_id"
+  add_index "duplicates_racers", ["racer_id", "duplicate_id"], :name => "index_duplicates_racers_on_racer_id_and_duplicate_id", :unique => true
+  add_index "duplicates_racers", ["racer_id"], :name => "index_duplicates_racers_on_racer_id"
 
   create_table "editor_requests", :force => true do |t|
     t.integer  "lock_version", :default => 0, :null => false
@@ -235,11 +247,32 @@ ActiveRecord::Schema.define(:version => 20110806162623) do
   add_index "events", ["date"], :name => "idx_date"
   add_index "events", ["discipline"], :name => "idx_disciplined"
   add_index "events", ["number_issuer_id"], :name => "events_number_issuer_id_index"
+  add_index "events", ["parent_id"], :name => "parent_id"
   add_index "events", ["promoter_id"], :name => "index_events_on_promoter_id"
   add_index "events", ["sanctioned_by"], :name => "index_events_on_sanctioned_by"
   add_index "events", ["type"], :name => "idx_type"
   add_index "events", ["type"], :name => "index_events_on_type"
   add_index "events", ["velodrome_id"], :name => "velodrome_id"
+
+  create_table "events_people", :id => false, :force => true do |t|
+    t.integer  "event_id"
+    t.integer  "person_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "events_people", ["event_id"], :name => "index_events_people_on_event_id"
+  add_index "events_people", ["person_id"], :name => "index_events_people_on_person_id"
+
+  create_table "events_promoters", :id => false, :force => true do |t|
+    t.integer  "event_id"
+    t.integer  "promoter_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "events_promoters", ["event_id"], :name => "index_events_promoters_on_event_id"
+  add_index "events_promoters", ["promoter_id"], :name => "index_events_promoters_on_promoter_id"
 
   create_table "historical_names", :force => true do |t|
     t.integer  "team_id",                     :null => false
@@ -252,6 +285,7 @@ ActiveRecord::Schema.define(:version => 20110806162623) do
 
   add_index "historical_names", ["name"], :name => "index_names_on_name"
   add_index "historical_names", ["team_id"], :name => "team_id"
+  add_index "historical_names", ["year"], :name => "index_names_on_year"
 
   create_table "images", :force => true do |t|
     t.string   "caption"
@@ -309,7 +343,7 @@ ActiveRecord::Schema.define(:version => 20110806162623) do
     t.text     "description"
   end
 
-  add_index "mailing_lists", ["name"], :name => "idx_mailing_lists_name"
+  add_index "mailing_lists", ["name"], :name => "idx_name"
 
   create_table "names", :force => true do |t|
     t.integer  "nameable_id",                  :null => false
@@ -323,6 +357,8 @@ ActiveRecord::Schema.define(:version => 20110806162623) do
     t.string   "last_name"
   end
 
+  add_index "names", ["name"], :name => "index_names_on_name"
+  add_index "names", ["nameable_id"], :name => "team_id"
   add_index "names", ["nameable_type"], :name => "index_names_on_nameable_type"
   add_index "names", ["year"], :name => "index_names_on_year"
 
@@ -467,6 +503,7 @@ ActiveRecord::Schema.define(:version => 20110806162623) do
   end
 
   add_index "pages", ["created_by_id"], :name => "index_pages_on_created_by_id"
+  add_index "pages", ["parent_id"], :name => "parent_id"
   add_index "pages", ["path"], :name => "index_pages_on_path", :unique => true
   add_index "pages", ["slug"], :name => "index_pages_on_slug"
 
@@ -537,6 +574,7 @@ ActiveRecord::Schema.define(:version => 20110806162623) do
   add_index "people", ["created_by_id"], :name => "index_people_on_created_by_id"
   add_index "people", ["crypted_password"], :name => "index_people_on_crypted_password"
   add_index "people", ["email"], :name => "index_people_on_email"
+  add_index "people", ["first_name"], :name => "idx_first_name"
   add_index "people", ["last_name"], :name => "idx_last_name"
   add_index "people", ["license"], :name => "index_people_on_license"
   add_index "people", ["login"], :name => "index_people_on_login"
@@ -546,6 +584,7 @@ ActiveRecord::Schema.define(:version => 20110806162623) do
   add_index "people", ["persistence_token"], :name => "index_people_on_persistence_token"
   add_index "people", ["print_card"], :name => "index_people_on_print_card"
   add_index "people", ["single_access_token"], :name => "index_people_on_single_access_token"
+  add_index "people", ["team_id"], :name => "idx_team_id"
 
   create_table "people_people", :id => false, :force => true do |t|
     t.integer "person_id", :null => false
@@ -577,6 +616,7 @@ ActiveRecord::Schema.define(:version => 20110806162623) do
   end
 
   add_index "posts", ["date", "mailing_list_id"], :name => "idx_date_list"
+  add_index "posts", ["date"], :name => "idx_date"
   add_index "posts", ["mailing_list_id"], :name => "idx_mailing_list_id"
   add_index "posts", ["sender"], :name => "idx_sender"
   add_index "posts", ["subject"], :name => "idx_subject"
@@ -602,6 +642,7 @@ ActiveRecord::Schema.define(:version => 20110806162623) do
   end
 
   add_index "promoters", ["name", "email", "phone"], :name => "promoter_info", :unique => true
+  add_index "promoters", ["name"], :name => "idx_name"
 
   create_table "race_numbers", :force => true do |t|
     t.integer  "person_id",        :default => 0,  :null => false
@@ -621,10 +662,50 @@ ActiveRecord::Schema.define(:version => 20110806162623) do
   add_index "race_numbers", ["value"], :name => "race_numbers_value_index"
   add_index "race_numbers", ["year"], :name => "index_race_numbers_on_year"
 
+  create_table "racers", :force => true do |t|
+    t.string   "first_name",          :limit => 64
+    t.string   "last_name"
+    t.string   "city",                :limit => 128
+    t.date     "date_of_birth"
+    t.string   "license",             :limit => 64
+    t.text     "notes"
+    t.string   "state",               :limit => 64
+    t.integer  "team_id"
+    t.integer  "lock_version",                       :default => 0,     :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "cell_fax"
+    t.string   "ccx_category"
+    t.string   "dh_category"
+    t.string   "email"
+    t.string   "gender",              :limit => 2
+    t.string   "home_phone"
+    t.string   "mtb_category"
+    t.date     "member_from"
+    t.string   "occupation"
+    t.string   "road_category"
+    t.string   "street"
+    t.string   "track_category"
+    t.string   "work_phone"
+    t.string   "zip"
+    t.date     "member_to"
+    t.boolean  "print_card",                         :default => false
+    t.boolean  "print_mailing_label",                :default => false
+    t.boolean  "ccx_only",                           :default => false, :null => false
+    t.string   "updated_by"
+    t.string   "bmx_category"
+    t.boolean  "wants_email",                        :default => true,  :null => false
+    t.boolean  "wants_mail",                         :default => true,  :null => false
+  end
+
+  add_index "racers", ["first_name"], :name => "idx_first_name"
+  add_index "racers", ["last_name"], :name => "idx_last_name"
+  add_index "racers", ["team_id"], :name => "idx_team_id"
+
   create_table "races", :force => true do |t|
     t.integer  "category_id",                                   :null => false
     t.string   "city",           :limit => 128
-    t.integer  "distance"
+    t.string   "distance"
     t.string   "state",          :limit => 64
     t.integer  "field_size"
     t.integer  "laps"
@@ -642,6 +723,7 @@ ActiveRecord::Schema.define(:version => 20110806162623) do
   end
 
   add_index "races", ["bar_points"], :name => "index_races_on_bar_points"
+  add_index "races", ["category_id"], :name => "idx_category_id"
   add_index "races", ["event_id"], :name => "index_races_on_event_id"
 
   create_table "racing_associations", :force => true do |t|
@@ -680,7 +762,7 @@ ActiveRecord::Schema.define(:version => 20110806162623) do
     t.string   "exempt_team_categories",                             :default => "0",                                   :null => false
     t.string   "membership_email",                                   :default => "scott.willson@gmail.com",             :null => false
     t.string   "name",                                               :default => "Cascadia Bicycle Racing Association", :null => false
-    t.string   "rails_host",                                         :default => "localhost:8080"
+    t.string   "rails_host",                                         :default => "localhost:3000"
     t.string   "sanctioning_organizations"
     t.string   "short_name",                                         :default => "CBRA",                                :null => false
     t.string   "show_events_sanctioning_org_event_id",               :default => "0",                                   :null => false
@@ -691,7 +773,7 @@ ActiveRecord::Schema.define(:version => 20110806162623) do
     t.datetime "updated_at"
     t.date     "cat4_womens_race_series_end_date"
     t.boolean  "unregistered_teams_in_results",                      :default => true,                                  :null => false
-    t.string   "domain"
+    t.date     "next_year_start_at"
   end
 
   create_table "refunds", :force => true do |t|
@@ -744,8 +826,8 @@ ActiveRecord::Schema.define(:version => 20110806162623) do
     t.string   "category_class",          :limit => 16
     t.string   "age_group",               :limit => 16
     t.text     "custom_attributes"
-    t.boolean  "competition_result",                                       :default => true, :null => false
-    t.boolean  "team_competition_result",                                  :default => true, :null => false
+    t.boolean  "competition_result",                                       :null => false
+    t.boolean  "team_competition_result",                                  :null => false
     t.string   "category_name"
     t.string   "event_date_range_s",                                       :null => false
     t.date     "date",                                                     :null => false
@@ -764,8 +846,10 @@ ActiveRecord::Schema.define(:version => 20110806162623) do
   add_index "results", ["category_id"], :name => "idx_category_id"
   add_index "results", ["event_id"], :name => "index_results_on_event_id"
   add_index "results", ["members_only_place"], :name => "index_results_on_members_only_place"
+  add_index "results", ["person_id"], :name => "idx_racer_id"
   add_index "results", ["place"], :name => "index_results_on_place"
   add_index "results", ["race_id"], :name => "idx_race_id"
+  add_index "results", ["team_id"], :name => "idx_team_id"
   add_index "results", ["year"], :name => "index_results_on_year"
 
   create_table "roles", :force => true do |t|
@@ -788,6 +872,25 @@ ActiveRecord::Schema.define(:version => 20110806162623) do
   add_index "scores", ["competition_result_id"], :name => "scores_competition_result_id_index"
   add_index "scores", ["source_result_id"], :name => "scores_source_result_id_index"
 
+  create_table "standings", :force => true do |t|
+    t.integer  "event_id",                              :default => 0,    :null => false
+    t.integer  "bar_points",                            :default => 1
+    t.string   "name"
+    t.integer  "lock_version",                          :default => 0,    :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "ironman",                               :default => true
+    t.integer  "position",                              :default => 0
+    t.string   "discipline",              :limit => 32
+    t.string   "notes",                                 :default => ""
+    t.integer  "source_id"
+    t.string   "type",                    :limit => 32
+    t.boolean  "auto_combined_standings",               :default => true
+  end
+
+  add_index "standings", ["event_id"], :name => "event_id"
+  add_index "standings", ["source_id"], :name => "source_id"
+
   create_table "teams", :force => true do |t|
     t.string   "name",                                :default => "",    :null => false
     t.string   "city",                :limit => 128
@@ -808,6 +911,7 @@ ActiveRecord::Schema.define(:version => 20110806162623) do
   end
 
   add_index "teams", ["created_by_id"], :name => "index_teams_on_created_by_id"
+  add_index "teams", ["name"], :name => "idx_name", :unique => true
 
   create_table "update_requests", :force => true do |t|
     t.integer  "lock_version",    :default => 0, :null => false
@@ -830,6 +934,8 @@ ActiveRecord::Schema.define(:version => 20110806162623) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["username"], :name => "idx_alias", :unique => true
 
   create_table "velodromes", :force => true do |t|
     t.string   "name"
