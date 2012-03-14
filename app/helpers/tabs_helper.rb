87ef3ed48150016e24aea3fab2628b4de01c27b5
Scott Willson <scott.willson@gmail.com>
2012-03-13 18:47:58 -0700

    Use striped table for results

--- app/helpers/tabs_helper.rb
+++ app/helpers/tabs_helper.rb
@@ -18,7 +18,7 @@ module TabsHelper
     end
 
     def add(name, options = {}, html_options = {}, &block)
-      _html_options = { :onmouseover => "hover(this)", :onmouseout => "hoverOut(this)" }
+      _html_options = { :class => "btn" }
       _html_options.merge!(html_options) if html_options
       @tabs << Tab.new(name, options, _html_options)
     end
@@ -33,60 +33,13 @@ module TabsHelper
       table_class = "tabs"
       table_class = "tabs_solo" if @tabs.size < 2
       html = <<HTML
-    <table class="#{table_class} centered">
-      <tr>
+     <div class="row"><div class="btn-group">
 HTML
       @tabs.each_with_index do |tab, index|
-        if index == 0
-          if select_current_page && current_page?(tab.options)
-            html << "      <td class=\"first_selected\"><div class=\"first_selected\"><span>"
-          else
-            html << "      <td class=\"first\"><div class=\"first\"><span>"
-          end
-          if select_current_page
-            html << link_to_unless_current(tab.name, tab.options, tab.html_options).to_s
-          else
-            html << link_to(tab.name, tab.options, tab.html_options).to_s
-          end
-          html << "</span></div>"
-          if @tabs.size < 2
-            if select_current_page && current_page?(tab.options)
-              html << "</td>\n      <td class=\"last_selected\"><div class=\"last_selected\">"
-              html << "<span>&nbsp;</span></div>"
-            else
-              html << "</td>\n      <td class=\"last\"><div class=\"last\"><span>&nbsp;</span></div>"
-            end
-          end
-        elsif index == @tabs.size - 1
-          if select_current_page && current_page?(tab.options)
-            html << "      <td class=\"last_selected\"><div class=\"last_selected\"><span>"
-          else
-            html << "      <td class=\"last\"><div class=\"last\"><span>"
-          end
-          if select_current_page
-            html << link_to_unless_current(tab.name, tab.options, tab.html_options).to_s
-          else
-            html << link_to(tab.name, tab.options, tab.html_options).to_s
-          end
-          html << "</span></div>"
-        else
-          if select_current_page && current_page?(tab.options)
-            html << "      <td class=\"selected\"><div class=\"selected\"><span>"
-          else
-            html << "      <td><div><span>"
-          end
-          if select_current_page
-            html << link_to_unless_current(tab.name, tab.options, tab.html_options).to_s
-          else
-            html << link_to(tab.name, tab.options, tab.html_options).to_s
-          end
-          html << "</span></div>"
-        end
-        html << "</td>\n"
+        html << link_to(tab.name, tab.options, tab.html_options).to_s
       end
       end_html = <<HTML
-      </tr>
-    </table>
+    </div></div>
 HTML
       html << end_html
     end
